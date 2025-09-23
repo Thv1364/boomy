2b233e635a43fbe543865aeccb9d8c577595ba5e
Thv1364 <144828312+Thv1364@users.noreply.github.com>
2025-09-23 17:56:31 +0000

    more fixing to script.js

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ gravity:320
 })
 
 k.loadbean();
+
+k.scene("main", () => {
+
+    // Add the player character ONLY
+    const player = k.add([
+        k.sprite("bean"),
+        k.pos(100, 100),
+        k.area(),
+        k.body(),
+        "player"
+    ]);
+// Movement controls are the same
+    k.onKeyDown("left", () => { player.move(-200, 0); });
+    k.onKeyDown("right", () => { player.move(200, 0); });
+    k.onKeyPress("space", () => {
+        if (player.isGrounded()) {
+            player.jump(400);
+        }
+    });
+
+});
+
+k.go("main");
